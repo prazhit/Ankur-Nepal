e15bcf389eee955930debc8b69d2fb4e7d690616
Prazhit <disarp969@gmail.com>
2019-11-28 13:03:45 +0545

    site updated

--- profile.php
+++ profile.php
@@ -5,9 +5,77 @@
 <section class="breadcrumbs member">
     <div class="container">
         <h1 class="display-4">Profile</h1>
-        <h5 class="font-weight-bold text-white mt-3"><a class="mr-2" href="index.php">Home</a> / <a class="mr-2" href="member.php">Membership</a> / <span class="span ml-2">Member 01</span></h5>
+        <h5 class="font-weight-bold text-white mt-3"><a class="mr-2" href="index.php">Home</a> / <a class="mr-2" href="member.php">Membership</a> / <span class="span ml-2">Profile 01</span></h5>
     </div>
+</section>
 
+
+<section class="profile py-5">
+    <div class="container d-flex align-items-center flex-wrap">
+        <img src="pics/about-2.jpg" alt="#" class="mr-5">
+        <div class="name py-5">
+            <h2 class="font-weight-bold text-white">Ankur</h2>
+            <p class="text-white"><i>@ankur</i> / 2 years ago</p>
+        </div>
+        
+    </div>
+</section>
+
+<section class="profile-table py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 col-md-7">
+                <table class="table table-striped">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Name</th>
+                            <td>Ankur</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Location</th>
+                            <td>Kathmandu</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Age</th>
+                            <td>25</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">ID</th>
+                            <td>@ankur</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Maritial Status</th>
+                            <td>Married</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <div class="comment-box mt-4">
+                    <div class="row no-gutters">
+                        <div class="col-lg-3 py-5 text-center">
+                            <img src="pics/about-2.jpg" alt="#" class="img-thumbnail">
+                        </div>
+                        <div class="col-lg-9 py-5">
+                            <div class="cmt p-4">
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus vel voluptatum dolorum esse quod, reprehenderit suscipit velit aut non id error, laboriosam sapiente temporibus reiciendis doloribus! Natus quisquam temporibus totam?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 pl-5 social offset-lg-1">
+                <h4 class="font-weight-bold">Social Links :</h4>
+                <div class="social-icons my-4">
+                    <ul class="list-unstyled d-flex align-items-center">
+                        <li><a href="#"><i class="fab mx-3 fa-facebook-f"></i></a></li>
+                        <li><a href="#"><i class="fab mx-3 fa-twitter"></i></a></li>
+                        <li><a href="#"><i class="fab mx-3 fa-linkedin-in"></i></a></li>
+                        <li><a href="#"><i class="fab mx-3 fa-google-plus-g"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 
 
