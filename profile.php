502bbe64f28c337a95f7802d5d240241d51d1df0
Prazhit <disarp969@gmail.com>
2019-11-28 11:07:45 +0545

    profile added

---
+++ profile.php
@@ -0,0 +1,30 @@
+<?php include('include/header.php');?>
+
+<!-- ======================================= Breadcrumbs =========================== -->
+
+<section class="breadcrumbs member">
+    <div class="container">
+        <h1 class="display-4">Profile</h1>
+        <h5 class="font-weight-bold text-white mt-3"><a class="mr-2" href="index.php">Home</a> / <a class="mr-2" href="member.php">Membership</a> / <span class="span ml-2">Member 01</span></h5>
+    </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include('include/footer.php');?>
